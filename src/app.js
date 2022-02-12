fbe192f46102b58ea78b0bc017915b2d04e9ae21
MrsMags17 <95896184+MrsMags17@users.noreply.github.com>
2022-02-11 21:31:21 -0600

    added axios js

--- src/app.js
+++ src/app.js
@@ -61,6 +61,12 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "36af1b9b85f7a9caf5e88d1e7d96d3b5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
